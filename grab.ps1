9f4bc19d3d41ad5113c1a501ba2bbc567f786874
unknown <robineatcorn@gmail.com>
2019-07-15 21:14:27 +0800

    grab.ps1:     如果node grab.js抓取不成功，会重新抓取一次；最多抓取5次。

---
+++ grab.ps1
@@ -0,0 +1,11 @@
+$timesLeft=5;
+$exit_code=1;
+
+cd $PSScriptRoot;
+
+while(($timesLeft -gt 0) -and ($exit_code -ne 0)){
+    node ./grab.js
+    $exit_code=$LASTEXITCODE;
+    Write-Host Exited with $exit_code ,times left $timesLeft
+    $timesLeft-=1;
+}
